df5ede245eb986698dd636f5a6579fbd122c548b
teja-devopspractice <surlaraviteja60@gmail.com>
2024-10-01 16:38:40 +0530

    special variables

--- 03-variable.sh
+++ 03-variable.sh
@@ -14,3 +14,9 @@ ARTH=$((2-3*4/2))
 
 echo ARTH = ${ARTH}
 
+# Special Variables For Inputs
+ echo script name - $0
+ echo first argument - $1
+ echo second argument - $2
+ echo all argument - "$*"
+ echo no of arguments - "$#"
